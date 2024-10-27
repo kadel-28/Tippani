df897aebe10def32106842e9bf57a9b8650b7da2
kadel-28 <149813234+kadel-28@users.noreply.github.com>
2024-10-27 09:55:13 +0545

    Edit Note API made

--- server/controllers/noteControllers.js
+++ server/controllers/noteControllers.js
@@ -41,6 +41,47 @@ const addNote = async(req,res)=>{
     }
 }
 
+const editNote = async(req, res)=>{
+    const noteId= req.params.noteId;
+    const {title, content, tags, isPinned}=req.body;
+    const {user}=req.user;
+
+    if (!title && !content ) {
+        return res.status(400).json({
+            error: true,
+            message: "Fields cannot be left empty."
+        })
+    }
+
+
+    try {
+        const note = await Note.findOne({_id:noteId, userId:user._id});
+
+        if (!note){
+            return res.status(404).json({
+                error: true,
+                message:"Note not Found"
+            });
+        }
+        if (title) note.title=title;
+        if (content) note.content=content;
+        if (tags) note.tags=tags;
+        if (isPinned) note.isPinned=isPinned;
+
+        await note.save();
+
+        return res.status(200).json({
+            error: false,
+            message:"Notes updated Successfully"
+        });
+        
+    } catch (error) {
+        return res.status(500).json({ error: errorHandler(error) });
+    }
+}
+
+
 module.exports={
     addNote,
+    editNote
 }
--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -2,9 +2,11 @@ const express=require('express');
 const {authenticateToken}=require('../middleware/auth-middleware')
 const router= express.Router();
 const {
-    addNote
+    addNote,
+    editNote
 }=require('../controllers/noteControllers')
 
 router.post('/addNote', authenticateToken, addNote);
+router.put('/editNote/:noteId', authenticateToken, editNote);
 
 module.exports=router;
